6fc4003e0c63a8a7e1c279d54e6b1a923e097b02
joncoronel <59675970+joncoronel@users.noreply.github.com>
2024-03-07 20:52:04 -0800

    Fix import path for Next UI theme components

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ module.exports = {
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./node_modules/@nextui-org/theme/dist/components/(button|input|spinner).js",
+    // "./node_modules/@nextui-org/theme/dist/components/(button|input|spinner).js",
   ],
   theme: {
     extend: {
@@ -160,7 +160,7 @@ module.exports = {
             textShadow: value,
           }),
         },
-        { values: theme("textShadow") }
+        { values: theme("textShadow") },
       );
     }),
   ],
