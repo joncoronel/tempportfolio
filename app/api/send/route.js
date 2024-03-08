b7589d848c5d919e576d2c8912f6c4f95dd0ce8a
joncoronel <59675970+joncoronel@users.noreply.github.com>
2024-03-07 20:45:19 -0800

    Refactor email sending code in send route

--- app/api/send/route.js
+++ app/api/send/route.js
@@ -2,41 +2,27 @@ import { NextResponse } from "next/server";
 import emailjs from "@emailjs/nodejs";
 
 export async function POST(request) {
-  const { name, email, message } = await request.json();
-
-  const templateParams = {
-    name,
-    email,
-    message,
-  };
-
-  const serviceID = process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID;
-  const templateID = process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID;
-
-  const publicKey = process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY;
-  const privateKey = process.env.NEXT_PUBLIC_EMAILJS_PRIVATE_KEY;
-  console.log("serviceID", serviceID);
-
   try {
-    emailjs
-      .send(serviceID, templateID, templateParams, {
-        publicKey: publicKey,
-        privateKey: privateKey, // optional, highly recommended for security reasons
-      })
-      .then(
-        (response) => {
-          console.log("SUCCESS!", response.status, response.text);
-          return new NextResponse("Email sent", { status: 200 });
-        },
-        (err) => {
-          console.log("FAILED...", err);
-          return new NextResponse("Error sending email", { status: 500 });
-        },
-      );
+    const { name, email, message } = await request.json();
+    const templateParams = { name, email, message };
 
+    const serviceID = process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID;
+    const templateID = process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID;
+
+    const publicKey = process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY;
+    const privateKey = process.env.NEXT_PUBLIC_EMAILJS_PRIVATE_KEY;
+
+    const response = await emailjs.send(serviceID, templateID, templateParams, {
+      publicKey: publicKey,
+      privateKey: privateKey,
+    });
+
+    console.log("Email sent successfully", response.status, response.text);
     return new NextResponse("Email sent", { status: 200 });
   } catch (error) {
     console.error("Error sending email", error);
-    return new NextResponse("Error sending email", { status: 500 });
+    return new NextResponse("Error sending email: " + error.message, {
+      status: 500,
+    });
   }
 }
