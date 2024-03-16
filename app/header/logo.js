8ca94a0d4c0bbd129c91e5650ecad2f7d18317bc
joncoronel <59675970+joncoronel@users.noreply.github.com>
2024-03-16 01:33:17 -0700

    Make logo priority

--- app/header/logo.js
+++ app/header/logo.js
@@ -3,11 +3,12 @@ import logo from "@/public/images/portfolio.svg";
 
 export default function Logo() {
   return (
-    <div className="relative flex-1 flex h-8 items-center justify-start">
+    <div className="relative flex h-8 flex-1 items-center justify-start">
       <Image
         src={logo}
         alt="Portfolio Logo"
         className="h-full w-auto drop-shadow-logo-glow "
+        priority
       />
     </div>
   );
