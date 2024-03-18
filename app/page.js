058070cf41f920c23dd7f33f82ea2fc472dfcd4f
joncoronel <59675970+joncoronel@users.noreply.github.com>
2024-03-18 13:04:24 -0700

    Update background gradient in Home section

--- app/page.js
+++ app/page.js
@@ -7,7 +7,7 @@ import Sticky from "./components/sticky";
 export default function Home() {
   return (
     <div className="mb-[5rem] flex flex-col items-center justify-between lg:mb-[10rem]">
-      <section className="flex w-full flex-col bg-gradient-radial from-primary-900 to-primary-800 px-4">
+      <section className="flex w-full flex-col bg-gradient-radial from-primary-500 to-primary-800 px-4">
         <Hero />
       </section>
       <section className="mb-[8rem] mt-16 flex w-full max-w-7xl flex-col px-4">
