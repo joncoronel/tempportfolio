b42219fce31f72eb5c5906b836c9e9aa0118d717
joncoronel <59675970+joncoronel@users.noreply.github.com>
2024-03-18 12:19:51 -0700

    performance for sticky component optimization

--- app/components/sticky.js
+++ app/components/sticky.js
@@ -1,29 +1,31 @@
 "use client";
 
-import { useRef, useEffect, useState } from "react";
-import styles from "./sticky.module.css";
+import { useRef } from "react";
+// import styles from "./sticky.module.css";
 
-function isScrollAnimationNotSupported() {
-  return (
-    !("animation" in document.documentElement.style) ||
-    typeof ScrollTimeline === "undefined"
-  );
-}
+import { motion, useScroll, useTransform, easeOut } from "framer-motion";
+
+// function isScrollAnimationNotSupported() {
+//   return (
+//     !("animation" in document.documentElement.style) ||
+//     typeof ScrollTimeline === "undefined"
+//   );
+// }
 
 function MotionSticky({ title, index, children, motion }) {
   const ref = useRef(null);
 
-  const { scrollYProgress } = motion.useScroll({
+  const { scrollYProgress } = useScroll({
     target: ref,
     offset: ["end start", "1 .7"],
   });
 
-  const y = motion.useTransform(scrollYProgress, [1, 0], [0, 200]);
+  const y = useTransform(scrollYProgress, [1, 0], [0, 200]);
 
-  const opacity = motion.useTransform(scrollYProgress, [1, 0], [1, 0]);
+  const opacity = useTransform(scrollYProgress, [1, 0], [1, 0]);
 
   return (
-    <motion.motion.div
+    <motion.div
       ref={ref}
       className={`relative scale-100`}
       style={{
@@ -37,42 +39,14 @@ function MotionSticky({ title, index, children, motion }) {
       id={title}
     >
       {children}
-    </motion.motion.div>
+    </motion.div>
   );
 }
 
 export default function Sticky({ title, index, children }) {
-  const [motion, setMotion] = useState(null);
-
-  useEffect(() => {
-    async function loadMotion() {
-      if (true) {
-        const { motion, useScroll, useTransform, easeOut } = await import(
-          "framer-motion"
-        );
-        setMotion({ motion, useScroll, useTransform, easeOut });
-      }
-    }
-    loadMotion();
-  }, []);
-
-  if (motion) {
-    return (
-      <MotionSticky title={title} index={index} motion={motion}>
-        {children}
-      </MotionSticky>
-    );
-  }
-
   return (
-    <div
-      className={`relative scale-100 ${styles.sticky}`}
-      style={{
-        zIndex: index,
-      }}
-      id={title}
-    >
+    <MotionSticky title={title} index={index} motion={motion}>
       {children}
-    </div>
+    </MotionSticky>
   );
 }
