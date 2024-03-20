f21bb70036c479da422497b5dd0d4928fece9623
joncoronel <59675970+joncoronel@users.noreply.github.com>
2024-03-20 08:12:23 -0700

    Add lodash.debounce to optimize scroll handling in header component

--- app/header/header.js
+++ app/header/header.js
@@ -3,6 +3,7 @@ import { usePathname } from "next/navigation";
 import CharmingTabs from "../components/CharmingTabs";
 import { useEffect } from "react";
 import throttle from "lodash.throttle";
+import debounce from "lodash.debounce";
 
 const items = [
   { label: "About", href: "/" },
@@ -11,30 +12,41 @@ const items = [
 ];
 
 const handleScroll = () => {
-  const logoHeight = document.getElementById("Logo")?.offsetHeight;
-  const header = document.querySelector("header");
-  const translateYValue =
-    logoHeight +
-    parseFloat(getComputedStyle(document.documentElement).fontSize);
+  if (window.innerWidth < 640) {
+    const logoHeight = document.getElementById("Logo")?.offsetHeight;
+    const header = document.querySelector("header");
+    const translateYValue =
+      logoHeight +
+      parseFloat(getComputedStyle(document.documentElement).fontSize);
 
-  if (window.scrollY > logoHeight) {
-    header.style.transform = `translateY(-${translateYValue}px)`;
+    if (window.scrollY > logoHeight) {
+      header.style.transform = `translateY(-${translateYValue}px)`;
+    } else {
+      header.style.transform = "translateY(0)";
+    }
   } else {
+    const header = document.querySelector("header");
     header.style.transform = "translateY(0)";
   }
 };
+
+const handleResize = debounce(() => {
+  handleScroll();
+}, 300);
 
 export default function Header() {
   const pathname = usePathname();
   const selectedTab = items.find((item) => item.href === pathname)?.label;
 
   useEffect(() => {
-    if (typeof window !== "undefined" && window.innerWidth < 640) {
+    if (typeof window !== "undefined") {
       handleScroll();
       const throttledScroll = throttle(handleScroll, 100);
       window.addEventListener("scroll", throttledScroll);
+      window.addEventListener("resize", handleResize);
       return () => {
         window.removeEventListener("scroll", throttledScroll);
+        window.removeEventListener("resize", handleResize);
       };
     }
   }, []);
