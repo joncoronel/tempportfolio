d8a268c672a2e6134307612045421c6b179e82e9
joncoronel <59675970+joncoronel@users.noreply.github.com>
2024-03-27 23:09:55 -0700

    Refactor PulseButton styles for secondary variant and update ProjectCard styles

--- app/sections/projects/projectcard.js
+++ app/sections/projects/projectcard.js
@@ -6,8 +6,8 @@ import Overman from "../../../public/images/projects/overman.webp";
 
 export default function ProjectCard({ title, description, image }) {
   return (
-    <div className="flex min-h-[16rem] w-full max-w-4xl flex-col-reverse overflow-clip  rounded-3xl border-1 border-primary-500 bg-primary-700  shadow-card md:flex-row ">
-      <div className="justi flex flex-1 flex-col items-center justify-between gap-4 p-6">
+    <div className="flex  w-full max-w-4xl flex-col-reverse overflow-clip  rounded-xl border-1 border-primary-500 bg-primary-700  shadow-card md:flex-row ">
+      <div className="flex flex-1 flex-col items-center justify-between gap-1 p-4 sm:p-6">
         <div className="flex flex-row items-center  gap-4 self-end">
           <PulseButton
             aria-label={"Live version of website"}
@@ -22,8 +22,8 @@ export default function ProjectCard({ title, description, image }) {
           ></PulseButton>
         </div>
         <div className="flex w-full  flex-col gap-2">
-          <h2 className="text-2xl font-bold text-secondary-500">{title}</h2>{" "}
-          <p className="text-secondary-200">{description}</p>
+          <h2 className="text-xl font-bold text-secondary-500">{title}</h2>{" "}
+          <p className="text-sm text-secondary-200">{description}</p>
         </div>
 
         <Link
@@ -34,11 +34,11 @@ export default function ProjectCard({ title, description, image }) {
           Learn More <CircleChevronRight size={"1.1rem"} />
         </Link>
       </div>
-      <div className="relative flex items-end  justify-center overflow-clip bg-orange-500/30  px-6  md:min-w-[30rem]">
+      <div className="relative flex items-end  justify-center overflow-clip bg-orange-500/30  px-8  md:min-w-[30rem]">
         <Image
           src={Overman}
           alt={title}
-          className="flex w-full max-w-[20rem] translate-y-4 rounded-t-lg transition-transform hover:translate-y-6 md:max-w-[23rem]  md:translate-y-10"
+          className="flex w-full max-w-[20rem] translate-y-6 rounded-t-lg transition-transform md:max-w-[23rem] md:translate-y-10  md:hover:translate-y-6"
         />
       </div>
     </div>
