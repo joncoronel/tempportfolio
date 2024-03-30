a1ee5942a80984128852b0e2088fc58ded76aba8
joncoronel <59675970+joncoronel@users.noreply.github.com>
2024-03-30 13:22:16 -0700

    Remove Contact and Info page components

--- app/contact/page.js
+++
@@ -1,7 +0,0 @@
-export default function Contact() {
-  return (
-    <div className="flex  flex-col items-center justify-between p-24">
-      <h1>Contact</h1>
-    </div>
-  );
-}
--- app/info/page.js
+++
@@ -1,7 +0,0 @@
-export default function Info() {
-  return (
-    <div className="flex  flex-col items-center justify-between p-24">
-      <h1>Info Page</h1>
-    </div>
-  );
-}
