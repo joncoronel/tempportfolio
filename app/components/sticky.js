3a492752ede1d91b88a67d35587c0b2836dd982f
joncoronel <59675970+joncoronel@users.noreply.github.com>
2024-03-30 02:31:20 -0700

    Refactor Sticky component to remove unnecessary code and only use css animation

--- app/components/sticky.js
+++ app/components/sticky.js
@@ -1,49 +1,7 @@
-"use client";
-
-import { useRef } from "react";
-
-import { motion, useScroll, useTransform, easeOut } from "framer-motion";
-
-function MotionSticky({ title, index, children, motion }) {
-  const ref = useRef(null);
-
-  const { scrollYProgress } = useScroll({
-    target: ref,
-    offset: ["end start", "1 .45"],
-  });
-
-  const y = useTransform(scrollYProgress, [1, 0], [0, 150], { ease: easeOut });
-
-  const opacity = useTransform(scrollYProgress, [1, 0], [1, 0], {
-    ease: easeOut,
-  });
-
-  const scale = useTransform(scrollYProgress, [1, 0], [1, 0.99], {
-    ease: easeOut,
-  });
-
+export default function Sticky({ title, children }) {
   return (
-    <motion.div
-      ref={ref}
-      className={`relative stabilize-animation`}
-      style={{
-        opacity: opacity,
-        originY: 1,
-        y: y,
-        x: 0,
-        scale: scale,
-      }}
-      id={title}
-    >
+    <div className={`animation-sticky relative`} id={title}>
       {children}
-    </motion.div>
-  );
-}
-
-export default function Sticky({ title, index, children }) {
-  return (
-    <MotionSticky title={title} index={index} motion={motion}>
-      {children}
-    </MotionSticky>
+    </div>
   );
 }
