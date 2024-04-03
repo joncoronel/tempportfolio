d55affc2d1d7b4448e9172ae7aa445698079e928
joncoronel <59675970+joncoronel@users.noreply.github.com>
2024-04-03 02:19:58 -0700

    Update UI styles in navparent.js and page.js

--- app/page.js
+++ app/page.js
@@ -6,7 +6,7 @@ import About from "./sections/about/about";
 export default function Home() {
   return (
     <div className="mb-[5rem] flex flex-col items-center justify-between gap-52 !scroll-smooth lg:mb-[10rem]">
-      <section className="flex w-full flex-col items-center" id={"home"}>
+      <section className="flex w-full flex-col items-center pb-16" id={"home"}>
         <Hero />
       </section>
       <section className=" flex w-full max-w-5xl flex-col px-4" id={"about"}>
